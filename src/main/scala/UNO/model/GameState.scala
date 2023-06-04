2596bb36c4b8b350bc1767a4bee30e40d70e052a
ju391bihhtwgkn <bihl.julian@web.de>
2023-06-04 20:35:49 +0200

    included GameState

---
+++ src/main/scala/UNO/model/GameState.scala
@@ -0,0 +1,20 @@
+package UNO.model
+
+import UNO.cardComponent.cardBaseImp.Card
+import UNO.PlayerComponent.playerBaseImp.Player
+import UNO.stackComponent.stackBaseImp.Stack
+
+case class GameState(playerList: List[Player], playStack: List[Card]):
+
+  def getplayerList(): List[Player] =
+    playerList
+
+  def getstackCard(): Stack =
+    var stackCard = Stack(List(new Card("", ""))).initStack()
+    /*(1 to 100).foreach((i)=>{
+      stackCard = stackCard.shuffleCards()
+    })*/
+    stackCard
+
+  def getplayStack(): List[Card] =
+    playStack
